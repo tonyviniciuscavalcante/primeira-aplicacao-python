d1d73925dd2c5e99ca539fb7b550c27545942511
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-10-03 16:48:01 -0300

    Update exercicio01.py

---
+++ sabor-express/exercicio01.py
@@ -0,0 +1,17 @@
+print('Python na Escola de Programação da Alura')
+
+nome = 'Tony'
+idade = 24
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print("""
+A
+L
+U
+R
+A
+""")
+
+pi = 3.14159
+
