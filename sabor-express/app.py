2ed237566c48eaf1f8c1abbee386ce45069fca22
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2025-02-18 16:49:39 -0300

    Update app.py

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,5 +1,6 @@
 import os
 
+
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -10,37 +11,58 @@ def exibir_nome_do_programa():
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 """)
 
+
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
+
 def finalizar_app():
     os.system('clear')
     print('Finalizando o app\n')
 
+
+def opcao_invalida():
+    print('Opçāo inválida!\n')
+    input('Digite um tecla para voltar ao menu principal')
+    main()
+
+def cadastrar_novo_restaurante():
+    pass
+
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opçāo: '))
-    # opcao_escolhida = int(opcao_escolhida)
+    try:
+        opcao_escolhida = int(input('Escolha uma opçāo: '))
+        #opcao_escolhida = int(opcao_escolhida)
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+            cadastrar_novo_restaurante()
 
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
 
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
 
-    else:
-        finalizar_app()
+        elif opcao_escolhida == 4:
+            finalizar_app()
+
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 
 def main():
+    os.system('clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
+
 
 if __name__ == '__main__':
     main()
