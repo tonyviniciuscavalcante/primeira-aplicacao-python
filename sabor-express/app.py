e56e325884623fe199f402a46bd8bfbaf122b502
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2025-02-19 14:43:39 -0300

    aula 03 - completa

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,5 +1,6 @@
 import os
 
+restaurantes = ['Pizza', 'Sushi']
 
 def exibir_nome_do_programa():
     print("""
@@ -20,17 +21,41 @@ def exibir_opcoes():
 
 
 def finalizar_app():
-    os.system('clear')
-    print('Finalizando o app\n')
+    exibir_subtitulo('Finalizando o app')
+
+
+def voltar_ao_menu_principal():
+    input('\nDigite um tecla para voltar ao menu ')
+    main()
 
 
 def opcao_invalida():
     print('Opçāo inválida!\n')
-    input('Digite um tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
+
+
+def exibir_subtitulo(texto):
+    os.system('clear')
+    print(texto)
+    print()
+
 
 def cadastrar_novo_restaurante():
-    pass
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso')
+
+    voltar_ao_menu_principal()
+
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
 
 
 def escolher_opcao():
@@ -44,6 +69,7 @@ def escolher_opcao():
 
         elif opcao_escolhida == 2:
             print('Listar restaurantes')
+            listar_restaurantes()
 
         elif opcao_escolhida == 3:
             print('Ativar restaurante')
