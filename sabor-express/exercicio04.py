df350d2eedffbeeef76ff18c15cca1a2c70d1f43
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2025-02-21 14:57:53 -0300

    Create exercicio04.py

---
+++ sabor-express/exercicio04.py
@@ -0,0 +1,21 @@
+# 1 - Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade.
+
+
+
+# 2 - Utilizando o dicionário criado no item 1:
+# Modifique o valor de um dos itens no dicionário (por exemplo, atualize a idade da pessoa);
+# Adicione um campo de profissão para essa pessoa;
+# Remova um item do dicionário.
+
+
+
+# 3 - Crie um dicionário que relacione os números de 1 a 5 aos seus respectivos quadrados.
+
+
+
+# 4 - Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário.
+
+
+
+# 5 - Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionário.
+
